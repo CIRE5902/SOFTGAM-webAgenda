ae09bb5979e5f36e5c475bb77630d566a81024f2
EricPerez <ericperezvera02@gmail.com>
2024-03-11 11:37:31 +0100

    cambios en menu background y aplicado enlace en el header

--- vistas/partials/header.php
+++ vistas/partials/header.php
@@ -1,6 +1,6 @@
 <header class="header">
     <div class="header-logo">
-        <h1 class="logo">Nombre de la Clínica</h1>
+    <h1 class="logo"><a href="../../controladores/menuControlador.php">Nombre de la Clínica</a></h1>
     </div>
     <link rel="stylesheet" href="../css/header.css">
 </header>
