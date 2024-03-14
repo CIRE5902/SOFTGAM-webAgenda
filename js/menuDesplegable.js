c9c1d84b859623c95af81f0f71ebf63a579e6805
EricPerez <ericperezvera02@gmail.com>
2024-03-14 14:36:56 +0100

    durada corregida y estilo inputdate

--- js/menuDesplegable.js
+++ js/menuDesplegable.js
@@ -1,15 +1,10 @@
-// Obtén el checkbox y el cuerpo del documento
 var checkbox = document.querySelector('#check');
 var body = document.querySelector('body');
 
-// Escucha el evento de cambio en el checkbox
 checkbox.addEventListener('change', function() {
-    // Si el checkbox está marcado (menú desplegado)
     if (checkbox.checked) {
-        // Aplica overflow hidden al cuerpo del documento
         body.style.overflow = 'hidden';
     } else {
-        // Quita overflow hidden del cuerpo del documento
         body.style.overflow = 'visible';
     }
 });
